b05e3c95bbc16bccb6091c8436a8c2791d36488b
Stuart Kennedy <rav2040@gmail.com>
2023-04-10 15:46:05 +1200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,33 @@ import { context, getOctokit } from "@actions/github";
 
 async function main() {
     try {
-        console.log(process.env);
+        const token = getInput("token");
+        const tag = getInput("tag");
+        const message = getInput("message") || tag;
 
-        // const token = getInput("token");
-        // const tag = getInput("tag");
-        // const message = getInput("message") || tag;
+        const octokit = getOctokit(token);
+        const owner = context.repo.owner;
+        const repo = context.repo.repo;
 
-        // const octokit = getOctokit(token);
+        const createTagResponse = await octokit.rest.git.createTag({
+            owner,
+            repo,
+            tag,
+            message,
+            object: process.env.GITHUB_SHA!,
+            type: "commit",
+            tagger: {
+                name: "github-actions[bot]",
+                email: "41898282+github-actions[bot]@users.noreply.github.com",
+            },
+        });
 
-        // const createTagResponse = await octokit.rest.git.createTag({
-        //     owner: context.repo.owner,
-        //     repo: context.repo.repo,
-        //     object: context.,
-        //     tag,
-        //     message,
-        //     type: "commit",
-        //     tagger: {
-        //         name: "github-actions[bot]",
-        //         email: "41898282+github-actions[bot]@users.noreply.github.com",
-        //     },
-        // });
-
-        // await octokit.rest.git.createRef({
-        //     owner: context.repo.owner,
-        //     repo: context.repo.repo,
-        //     sha: createTagResponse.data.sha,
-        //     ref: "refs/tags/" + createTagResponse.data.tag,
-        // });
+        await octokit.rest.git.createRef({
+            owner,
+            repo,
+            sha: createTagResponse.data.sha,
+            ref: "refs/tags/" + createTagResponse.data.tag,
+        });
     } catch (err) {
         if (err instanceof Error) setFailed(err);
     }
